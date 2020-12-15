fc5e0fa989e9bdab0b76c337667dc9dd8beb1c6f
Allen Kuo <g01.webapp@gmail.com>
2020-12-15 22:40:27 +0800

    add:AutofacConfig.cs

---
+++ Src/Exec02.Solution/Exec02.FrontEnd.Site/App_Start/AutofacConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Exec02.FrontEnd.Application.News;
+using Exec02.FrontEnd.Domain.Interfaces;
+using Exec02.FrontEnd.Domain.News;
+using Exec02.FrontEnd.Infra.Data.EFModels;
+using Exec02.FrontEnd.Infra.Data.Repositories;
+
+namespace Exec02.FrontEnd.Site
+{
+	public class AutofacConfig
+	{
+		public static void Bootstrapper()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+			builder.RegisterType<HotNewsApi>().AsSelf();
+			builder.RegisterType<NewsService>().AsSelf();
+			builder.RegisterType<NewsRepository>().As<INewsRepository>();
+			builder.RegisterType<ErpDbContext>().AsSelf();
+			
+			builder.RegisterFilterProvider();
+
+			IContainer container = builder.Build();
+			DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+		}
+	}
+}
--- Src/Exec02.Solution/Exec02.FrontEnd.Site/Global.asax.cs
+++ Src/Exec02.Solution/Exec02.FrontEnd.Site/Global.asax.cs
@@ -18,6 +18,8 @@ namespace Exec02.FrontEnd.Site
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AutofacConfig.Bootstrapper();
         }
     }
 }
